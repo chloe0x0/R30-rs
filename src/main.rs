dec1fd5e095a5a1cbf2279d91ddb7c31987fe329
chloe0b0 <ChloeZeiter@gmail.com>
2022-08-25 14:48:58 -0700

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,26 @@
 mod r30;
-use r30::{R30, DEB};
+use r30::{DEB, R30};
 use std::time::SystemTime;
 
-fn main()
-{
+fn main() {
     let mut seed: u64 = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .expect("Duraction since UNIX_EPOCH Failed!!!")
         .as_secs();
-    seed *= seed;   // square the time
+    seed *= seed; // square the time
     let mut rng: R30 = R30::new(seed);
     print!("1\t\t|");
     rng.print();
-    for _n in 0..=50
-    {
+    for _n in 0..=50 {
         let bit: u64 = rng.rand_bit() as u64;
         print!("{}\t\t|", bit);
         rng.print();
     }
 
-    for n in 0..50
-    {
-        if (n+1) % 5 == 0
-        {
+    for n in 0..50 {
+        if (n + 1) % 5 == 0 {
             println!("{}", rng.rand_u64(5));
-        }
-        else
-        {
+        } else {
             print!("{}, ", rng.rand_u64(5));
         }
     }
--- src/r30.rs
+++ src/r30.rs
@@ -1,61 +1,49 @@
 pub const DEB: u64 = (1 as u64) << 31;
 const CELL_STR: &str = "\u{2588}";
 
-pub struct R30
-{
-    state: u64
+pub struct R30 {
+    state: u64,
 }
 
-impl R30
-{
-    pub fn new(seed: u64) -> Self
-    {
+impl R30 {
+    pub fn new(seed: u64) -> Self {
         R30 { state: seed }
     }
-    fn iterate(&mut self)
-    {
+    fn iterate(&mut self) {
         // pre-evolve the edge bit to avoid overflow on the most significant bit (leads to the state becoming u64 max)
-        let n: u64 = (self.state & ((1 as u64)))<<63 ^ (self.state & ((1 as u64) << 63)) | (self.state >> 1);
+        let n: u64 =
+            (self.state & (1 as u64)) << 63 ^ (self.state & ((1 as u64) << 63)) | (self.state >> 1);
         self.state = (self.state & !((1 as u64) << 63)) | (n << 63);
         self.state = (self.state >> 1) ^ (self.state | (self.state << 1));
     }
-    pub fn rand_bit(&mut self) -> bool
-    {
+    pub fn rand_bit(&mut self) -> bool {
         let bit: bool = self.state & ((1 as u64) << 31) != 0;
         self.iterate();
         return bit;
     }
-    pub fn rand_u64_in(&mut self, a: u64, b: u64) -> u64
-    {
+    pub fn rand_u64_in(&mut self, a: u64, b: u64) -> u64 {
         let y: u64 = self.rand_u64(64);
-        return y % (b+1 - a) + a;
+        return y % (b + 1 - a) + a;
     }
-    pub fn rand_u64(&mut self, bits: usize) -> u64
-    {
+    pub fn rand_u64(&mut self, bits: usize) -> u64 {
         let mut y: u64 = 0 as u64;
 
-        for n in 0..bits
-        {
+        for n in 0..bits {
             let bit: bool = self.rand_bit();
             y = (y & !((1 as u64) << n)) | ((bit as u64) << n);
         }
         self.iterate();
         return y;
     }
-    pub fn print(&self)
-    {
+    pub fn print(&self) {
         let mut string = String::new();
 
         let mut i = 63;
-        while i >= 0
-        {
+        while i >= 0 {
             let n = (self.state & ((1 as u64) << i) != 0) as u64;
-            if n != 0
-            {
+            if n != 0 {
                 string = string + CELL_STR;
-            }
-            else
-            {
+            } else {
                 string = string + " ";
             }
             i = i - 1;
